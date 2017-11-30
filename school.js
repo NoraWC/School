6ac2ae89e27baf1850ff14272723ed34aa70b760
Nora Coates <noracoates@students.berkeley.net>
2017-11-30 10:27:42 -0800

    hi

--- school.js
+++ school.js
@@ -17,7 +17,15 @@ function html() {
     var len = SECTIONS.length;
     var returnVal = "<table id = 'displaying'>";
     for(var i = 0; i < len; i ++) {
-        returnVal += "<tr><th id = " + SECTIONS[i].name + "><td></td></tr>"
+        var stu = [];
+        for (var x = 0; x < SECTIONS[i].students.length; x ++) {
+            stu += "<option value = '" + SECTIONS[i].students[x] + "'>" + SECTIONS[i].students[x] + "</option>";
+        }
+        returnVal += "<tr><th id = " + SECTIONS[i].name + ">";
+        returnVal += "<td id = 'teacher'>" + SECTIONS[i].teacher +"</td>";
+        returnVal += "<td id = 'students'><select id = 'studentsList" + SECTIONS[i] + "'>" + stu + "</td>";
+        returnVal += "<td id = 'searched'></td>";
+        returnVal += "</tr>";
     }
     returnVal += "</table>";
     document.getElementById("displayTable").innerHTML = returnVal;
@@ -27,19 +35,25 @@ function search() {
     var fn = document.getElementById("searchBarOne").value;//first name
     var ln = document.getElementById("searchBarTwo").value;//last name
     var id = document.getElementById("searchBarThree").value;//id
-    fn = fn.toLowerCase();
-    ln = ln.toLowerCase();
     var arr = [];
     for(var z in SECTIONS) {
-        var sect = SECTIONS[z];
-        for (var i in sect.students) {
-            if(sect.students[i].id === id || sect.students[i].lastName === ln || sect.students[i].firstName === fn) {
-                arr+=sect.students[i];
+        for (var i = 0; i < z.students; i++) {
+            if(z.students[i].id === id || z.students[i].lastName === ln || z.students[i].firstName === fn) {
+                arr+=z.students[i];
             }
         }
     }
-    //show/hide divs in table(display = none)
-    return arr;
+    //show/hide divs in table? (display = none; display = inline)
+    if(arr.length>0) {
+        document.getElementById('searched').innerHTML = arr.toString;
+    } else {
+        //gets current 'searched' innerhtml
+        var current = document.getElementById('searched').value;
+        //finds first close bracket
+        var stop = current.indexOf('>');
+        //should ideally replace first close bracket with class to hide td
+        document.getElementById('searched').innerHTML = current.substring(0, stop-1) + "class = 'secret'>" + current.substring(stop, current.length);
+    }
 }
 
 var Person = new Object();
@@ -58,7 +72,6 @@ Teacher.constructor = {
     subject : ""
 };
 
-
 var Section = new Object();
 Section.constructor = {
     name: "",
@@ -69,8 +82,11 @@ Section.constructor = {
     addStudent : function(Student) {
         this.students += Student;
     },
+    addTeacher: function(Teacher) {
+        this.teacher = Teacher;
+    },
     removeStudent: function(id) {
-        for(var i in this.students) {
+        for(var i = 0; i < this.students.length; i++) {
             if(this.students[i].id === id) {
                 this.students = this.students.slice(0,i) + this.students.slice(i+1,this.students.length);
             }
@@ -78,21 +94,36 @@ Section.constructor = {
     },
     sectionSeatsRemaining: function() {
         return this.maxSize - this.currentSize;
+    },
+    listInfo : function() {
+        var ret = "Name: " + this.name + " Max Size: " + this.maxSize + " Current Size: " + this.currentSize;
+        ret += " Students: " + this.students + " Teacher: " + this.teacher;
+        return ret;
     }
 };
 
-function addRemove() {
-
+function addStudentToSection(studentId, sectionId) {
+    sectionId.addStudent(studentId);
 }
 
-function newStudent() {
-
+function removeStudentFromSection(studentId, sectionId) {
+    sectionId.removeStudent(studentId);
 }
 
-function newTeacher() {
-
+function addaStudent(id, fname, lname, grade) {
+    var stu = new Student(id, fname, lname, grade);
+    this.addStudent(stu);
 }
 
-function newSection() {
+function addTeacher(id, fname, lname, subject) {
+    var teacher = new Teacher(id, fname, lname, subject);
+    this.addTeacher(teacher);
+}
 
+function addSection(sect, name, maxSize, currentSize, students, teacher) {
+    sect = new Section(name, maxSize, currentSize, students, teacher);
+    return sect;
+}
+function listSectionInfo(sectionId) {
+    sectionId.listInfo();
 }
