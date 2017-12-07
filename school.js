31356f47ca2b5dead33df923b945ba468fa207b1
Nora Coates <noracoates@students.berkeley.net>
2017-12-07 10:34:44 -0800

    hi

--- school.js
+++ school.js
@@ -10,6 +10,9 @@
 * OBJECTS
  */
 
+
+var STUDENTS = [];
+
 function Person() {
     this.id = 0;
     this.firstName = "";
@@ -33,6 +36,7 @@ function Section() {
     this.maxSize = 0;
     this.students = [];
     this.currentSize = this.students.length;
+
     this.addStudent = function(Student) {
         this.currentSize +=1;
         this.students.push(Student);
@@ -49,7 +53,7 @@ function Section() {
     };
     this.listInfo = function() {
         var ret = "Name: " + this.name + " Max Size: " + this.maxSize + " Current Size: " + this.currentSize;
-        ret += " Seats Remaining: " + this.sectionSeatsRemaining() +" Students: " + this.students + " Teacher: " + this.teacher;
+        ret += " Seats Remaining: " + this.sectionSeatsRemaining();
         return ret;
     }
 }
@@ -60,6 +64,7 @@ j.firstName = "Jane";
 j.lastName = "Doe";
 j.grade = 9;
 j.searched = false;
+STUDENTS += j;
 
 var g = new Student();
 g.id = 8006;
@@ -67,6 +72,7 @@ g.firstName = "Geordi";
 g.lastName = "Doe";
 g.grade = 11;
 g.searched = false;
+STUDENTS += g;
 
 var b = new Student();
 b.id = 1001;
@@ -74,6 +80,7 @@ b.firstName = "hi";
 b.lastName = "boo";
 b.grade = 12;
 b.searched = false;
+STUDENTS += b;
 
 var t = new Teacher();
 t.id = 1002;
@@ -82,7 +89,7 @@ t.lastName = "Potter";
 t.subject = "DADA";
 
 var sect = new Section();
-
+sect.id = 'sect';
 sect.name = "testsect";
 sect.maxSize = 40;
 sect.teacher = t;
@@ -93,6 +100,7 @@ sect.addStudent(b);
 
 var SECTIONS = [sect];//lists all sections
 
+
 function html() {
     //use length of sections to determine how many columns/rows needed?
     var len = SECTIONS.length;
@@ -101,15 +109,14 @@ function html() {
         var stu = [];
 
         for (var x = 0; x < SECTIONS[i].students.length; x ++) {
-            var anyStudent = SECTIONS[i].students[x];
+            var anyStu = SECTIONS[i].students[x];
             var classify = '';
-            if(!anyStudent.searched) {
+            if(!anyStu.searched) {
                 classify = 'secret';
             } else {
-                classify = 'stu';
+                classify = 'showStudent';
             }
-
-            stu += "<div class = '" + classify + "' id = 'student" + anyStudent.id + "'>" + anyStudent.firstName + " " + anyStudent.lastName + "</div>";
+            stu+="<div class = '"+classify+"' id = 'student"+anyStu.id+"'>"+anyStu.firstName+" "+anyStu.lastName+" "+anyStu.id+"</div>";
         }
         returnVal += "<tr><th id = " + SECTIONS[i].name + "><td id = 'name'>Section: " + SECTIONS[i].name + "</td>";
         returnVal += "<td id = 'teacher'> Teacher: " + SECTIONS[i].teacher.lastName +"</td>";
@@ -122,18 +129,20 @@ function html() {
 }
 
 function search() {
-    var fn = document.getElementById("searchBarOne").value;//first name
-    var ln = document.getElementById("searchBarTwo").value;//last name
-    var id = parseInt(document.getElementById("searchBarThree").value);//id
+    var fn = document.getElementById("stuInp1").value;//first name
+    var ln = document.getElementById("stuInp2").value;//last name
+    var id = parseInt(document.getElementById("stuInp3").value);//id
+    var gr = parseInt(document.getElementById("stuInp4").value);
     var f = false;
 
     for(var z =0; z < SECTIONS.length; z ++) {
         var stu = SECTIONS[z].students;
         for (var i = 0; i < stu.length; i++) {
-            if(stu[i].id === id || (stu.firstName === fn && stu.lastName === ln)){
+            if(stu[i].id === id || (stu[i].firstName === fn && stu[i].lastName === ln && stu[i].grade === gr)){
                 SECTIONS[z].students[i].searched = true;
                 f = true;
                 html();
+                return SECTIONS[z].students[i];
             }
         }
     }
@@ -143,28 +152,38 @@ function search() {
     }
 }
 
-function addStudentToSection() {
-    var section = document.getElementById("sectInp");
+function val(id) {
+    for (var geriatric = 0; geriatric < SECTIONS.length; geriatric ++) {
+        if(SECTIONS[geriatric].id == id) {
+            id = SECTIONS[geriatric];
+        }
+    }
+    return id;
+}
+
+function addStudentToSection(section) {//only takes the id; doesn't take the section
+    section = val(section);
     var newStu = new Student();
     newStu.firstName = document.getElementById("stuInp1").value;
     newStu.lastName = document.getElementById("stuInp2").value;
     newStu.id = parseInt(document.getElementById("stuInp3").value);
-    newStu.grade = document.getElementById("stuInp4").value;
+    newStu.grade = parseInt(document.getElementById("stuInp4").value);
     section.addStudent(newStu);
 }
 
-function removeStudentFromSection(section) {
-    //var section = document.getElementById("sectInp").value;
+function removeStudentFromSection(section) {//only takes the id; doesn't take the section
+    section = val(section);
     var fn = document.getElementById("stuInp1").value;
     var ln = document.getElementById("stuInp2").value;
-    var d = document.getElementById("stuInp3").value;
-    var g = document.getElementById("stuInp4").value;
-
-    for(var i =0; i < section.students.length; i++) {
-        var f = section.students[i].firstName;
-        if (f == fn && section.students[i].lastName == ln &&
-            section.students[i].id == parseInt(d) && section.students[i].grade == parseInt(g)) {
-            section.removeStudent(section.students[i]);
+    var d = parseInt(document.getElementById("stuInp3").value);
+    var g = parseInt(document.getElementById("stuInp4").value);
+    for(var num = 0; num < section.students.length; num++) {
+        var f = section.students[num].firstName;
+        var l = section.students[num].lastName;
+        var id = section.students[num].id;
+        var gr = section.students[num].grade;
+        if (f === fn && l === ln && id === parseInt(d) && gr === parseInt(g)) {
+            section.removeStudent(section.students[num]);
         }
     }
 }
@@ -198,5 +217,17 @@ function addSection(sect, name, maxSize, currentSize, students, teacher) {
     return sect;
 }
 function listSectionInfo(sectionId) {
-    sectionId.listInfo();
+    sectionId = val(sectionId);
+    var tea = " Teacher: Mx. ";
+    tea += sectionId.teacher.lastName + ", " + sectionId.teacher.subject;
+    var stu = " Students:";
+    for (var boo = 0; boo < sectionId.students.length; boo++) {
+        stu += " " + sectionId.students[boo].firstName + " " + sectionId.students[boo].lastName;
+        stu += " ID: " + sectionId.students[boo].id;
+        stu += " Grade: " + sectionId.students[boo].grade;
+        if(boo>0 && boo < sectionId.students.length-1) {
+            stu += "<br>";
+        }
+    }
+    return sectionId.listInfo() + tea + stu;
 }
