cb717493e5489e9f46f0ed71d3ad8ba8aea41514
Nora Coates <noracoates@students.berkeley.net>
2017-12-15 10:34:35 -0800

    hi

--- school.js
+++ school.js
@@ -129,41 +129,61 @@ other.addStudent(g);
 console.log(other);
 
 
+function showTeacher(bool) {
+    var classy = 'secret';
+    var returnVal = '';
+    var vals = ["'first name", "'last name", "'id", "'subject"];
+    if(bool) {
+        classy = 'show';
+    }
+    for(var y = 1; y < 5; y++) {
+        returnVal += "<input title = 'teacherInp'" + y + "' class = '"+ classy + "'";
+        returnVal += "' id = 'teacherInp'" + y + "' type = 'text' value = " + vals[y-1] + "'>";
+    }
+    return returnVal;
+}
+
+function showStudent(bool) {
+    var classy = 'secret';
+    var returnVal = '';
+    var vals = ["'first name", "'last name", "'id", "'grade"];
+    if(bool) {
+        classy = 'show';
+    }
+    for(var y = 1; y < 5; y++) {
+        returnVal += "<input title = 'studentInp'" + y + "' class = '"+ classy + "'";
+        returnVal += "' id = 'StudentInp'" + y + "' type = 'text' value = " + vals[y-1] + "'>";
+    }
+    return returnVal;
+}
+
+
+
+
 function html() {
     //use length of sections to determine how many columns/rows needed
     var len = SECTIONS.length;
     var returnVal = "<table>";
-    var arr = ["'student", "'teacher"];
-    var vals = ["'first name", "'last name", "'id"];
-    var extra = ["'grade", "'subject"];
-    var fire = "secret";
-    var classy = "show";
     var divs = "";
-    for (var z = 0; z < 2; z ++) {
-        divs += "<td><div id = " + arr[z] + "Inputs'>";
-        vals.push(extra[z]);
 
-        var fin = "";
-        fin += "<button id = 'show' onclick = \'fire = " + classy + "'></button>";
-        for(var y = 1; y < 5; y++) {
 
-            fin += "<input title = "+ arr[z] + "Inp" + y + "' class = '";
-            if(!test) {
-                classy = 'secret';
-            } else {
-                classy = 'show';
-            }
-            fin += "' id = " + arr[z] + "Inp" + y + "' type = 'text' value = " + vals[y-1] + "'>";
-        }
-        divs += fin + "</div></td>";
-    }
 
     var bleh = ["'name/id", "'max size", "'current size", "'students", "'teacher"];
-    divs += "<td><div>";
-    divs += "<button id = 'showsection' onclick = \' fire = " + classy+ "'</button>";
+    divs += "<td><div id = 'sectionInputs'>";
+    divs += "<button id = 'showsection' onclick = 'listSectionInfo();'></button>";
     for (var f = 1; f < 6; f ++) {
-        divs += "<input title = 'sectionInp" + f + "' class = '" + fire + "' id = 'sectionInp" + f + "' type = 'text' value = " + bleh[f-1] + "'>";
+        //fix class
+        //make a function like Student/Teacher???
+        divs += "<input title = 'sectionInp" + f + "' class = 'show' id = 'sectionInp" + f + "' type = 'text' value = " + bleh[f-1] + "'>";
     }
+
+    divs += "<td><div id = 'teacherInputs'>";
+    divs += "<button id = 'showTeacher' onclick = 'showTeacher(true);'></button>";
+    divs += "</div></td>";
+    divs += "<td><div id = 'studentInputs'>";
+    divs += "<button id = 'showStudent' onclick = 'showStudent(true);'></button>";
+    divs += "</div></td>";
+
      returnVal += "<tr>" + divs + "</div></td></tr>";
 
     for(var i = 0; i < len; i ++) {
